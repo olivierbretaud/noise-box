5b434c4ef7bf5f267d04a3836482e50d28168c7a
Olivier Bretaud <olivierbretaud@hotmail.fr>
2019-11-15 08:50:46 +0100

    first commit

---
+++ src/Components/Partials/Synth.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Synth(props) {
+  
+  function playNote() {
+    props.synth.triggerAttackRelease(props.note, "4n")
+  }
+
+  return (
+    <button className="button-play"
+      style={{ backgroundColor: props.color }}
+      onClick={() => playNote()}>
+      <span>{props.note}</span>
+    </button>
+  )
+} 
