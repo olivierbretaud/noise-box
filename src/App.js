9963abb3f4c4866d5e6d6cc74e9476a95ec82a8a
devFront <devfront@MacBook-Dev-Olivier.local>
2019-11-15 14:27:52 +0100

    set note

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React from 'react';
-import Tone from 'tone';
 import Synth from './Components/Partials/Synth';
 
 function App() {
 
-  var synthA = new Tone.Synth({
-    oscillator: {
-      type: 'fmsquare',
-      modulationType: 'sawtooth',
-      modulationIndex: 3,
-      harmonicity: 3.4
-    },
-    envelope: {
-      attack: 0.001,
-      decay: 0.1,
-      sustain: 0.1,
-      release: 0.1
-    }
-  }).toMaster()
-  
-  var synthB = new Tone.MembraneSynth().toMaster()
-
-  var Cmajor = ['A4','B4','C4','D4','E4','F4','G4'];
-
   return (
     <div className="App">
-      <div className="synth-container">
-        {Cmajor.map(note => <Synth synth={synthA} note={note} color={"#cc00cc"}/>)}
-      </div>
-      <div className="synth-container">
-        {Cmajor.map(note => <Synth synth={synthB} note={note} color={"#48D1CC"}/>)}
-      </div>
+      <Synth />
     </div>
   );
 }
--- src/Components/Partials/Synth.jsx
+++ src/Components/Partials/Synth.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Tone from 'tone';
+
+import Note from './Note';
 
 export default function Synth(props) {
-  
-  function playNote() {
-    props.synth.triggerAttackRelease(props.note, "4n")
+  const [noteValue , setNoteValue] = useState(1);
+
+  function handelChangeNote(note) {
+    if (note > 0 && note < 8) {
+      setNoteValue(note)
+    }
   }
+  
+  var synthA = new Tone.MembraneSynth().toMaster();
+
+  var Cmajor = ['A','B','C','D','E','F','G'];
 
   return (
-    <button className="button-play"
-      style={{ backgroundColor: props.color }}
-      onClick={() => playNote()}>
-      <span>{props.note}</span>
-    </button>
+    <>
+      <div className="synth-container">
+        {Cmajor.map(note => <Note synth={synthA} note={note + noteValue} color={"#cc00cc"}/>)}
+      </div>
+      <div className="synth-container">
+        {Cmajor.map(note => <Note synth={synthA} note={note + noteValue + '#'} color={"#cc00cc"}/>)}
+      </div>
+      <div>
+      <div className="synth-container">
+        <div className="btn-set-note" onClick={()=> handelChangeNote(noteValue - 1)}>-</div>
+        {noteValue}
+        <div className="btn-set-note" onClick={()=> handelChangeNote(noteValue + 1)}>+</div>
+      </div>
+      </div>
+    </>
   )
 } 
